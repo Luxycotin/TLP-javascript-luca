41995ba06b756d039e7c586b77ec2b20dd946a54
Luca Espinoza <luca.g.esp2005@gmail.com>
2024-05-10 10:07:55 -0300

    Se creo la estructura de un nuevo trabajp

---
+++ trabajo-Practico-2/assets/js/calificacion.js
@@ -0,0 +1,26 @@
+let tr1 = parseInt(prompt('Ingrese nota del primer trimestre'))
+let tr2 = parseInt(prompt('Ingrese nota del primer trimestre'))
+let tr3 = parseInt(prompt('Ingrese nota del primer trimestre'))
+let prom = (tr1 + tr2 + tr3)%3
+if((isNaN (tr1)) || (isNaN(tr2)) || (isNaN(tr3))){
+    console.log('alguno de los numeros ingresados no es valido');
+} else if ((tr1!=0) (tr2!=0) (tr3!=0) ){
+    switch(prom){
+        case :
+        console.log('')
+        break;
+        case :
+
+        break;
+        case :
+
+        break;
+        case :
+
+        break;
+        case 10:
+
+        break;
+        default
+    }
+}
