d044d60a7d917c8dd341f23cba0e77126dfefb90
Luca Espinoza <luca.g.esp2005@gmail.com>
2024-05-17 12:14:15 -0300

    Se creó la actividad Practica número 3

--- trabajo-Practico-2/assets/js/descuento.js
+++ trabajo-Practico-2/assets/js/descuento.js
@@ -7,7 +7,7 @@ switch(true){
     case total>=20000 && articulos>=10:
         console.log('Se le descontará  $',(descuento), ' de su total a pagar, que será de:  $',(total-descuento))
         break;
-
+        
         default:
             console.log('Usted no aplica para el descuento, su total es: $',(total))
             break;
